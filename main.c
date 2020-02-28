d818cb4b729218c9a3e44b6af77e56bec02212dd
Yanmii <yanmii@pm.me>
2020-02-28 16:59:56 +0000

    general: board creation w/ user input

--- main.c
+++ main.c
@@ -9,6 +9,17 @@ int main(int argc, char *argv[])
     printf("%d argument(s)\n", argc-1);
   }
 
-  mk_game(10, 10);
+  int n = 0;
+  while (n < 4)
+  {
+    printf("Choose the board size: ");
+    scanf("%d", &n);
+    if (n < 4)
+    {
+      printf("Invalid size, a board must be at least 4x4");
+    }
+  }
+
+  mk_game(n, n);
   return 0;
 }
