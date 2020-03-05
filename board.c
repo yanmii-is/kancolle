c3722acc0c632eec85e024b829e5036e51368827
Yanmii <yanmii@pm.me>
2020-03-05 19:38:16 +0000

    board: properly allocate memory

--- board.c
+++ board.c
@@ -6,7 +6,7 @@ Board* mk_board(int height, int width)
   Board* ret = malloc(sizeof(Board));
   ret->height = height;
   ret->width = width;
-  ret->matrix = (int**) malloc(width * height * sizeof(int));
+  ret->matrix = (int**) malloc(width * sizeof(int*));
   for (int i = 0; i < height; i++)
   {
     ret->matrix[i] = (int*) malloc(height * sizeof(int));
